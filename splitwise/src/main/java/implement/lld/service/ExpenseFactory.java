21dce67d20777d64d578fc5c945fd363aaf86e5b
Anupam Kumar <anupam.kumar@navi.com>
2025-02-14 01:10:24 +0530

    modify factory

--- splitwise/src/main/java/implement/lld/service/ExpenseFactory.java
+++ splitwise/src/main/java/implement/lld/service/ExpenseFactory.java
@@ -8,15 +8,19 @@ import implement.lld.model.split.Split;
 import java.util.List;
 import java.util.UUID;
 
-import static java.util.List.of;
-
 public class ExpenseFactory {
-    public static Expense createExpense(UUID payerId, String description, Double amount, List<Split> splits, ExpenseType expenseType, UUID groupId) {
+    public static Expense createExpense(
+        UUID payerId,
+        String description,
+        Double amount,
+        List<Split> splits,
+        ExpenseType expenseType,
+        UUID groupId
+    ) {
         return switch (expenseType) {
-            case GROUP -> new Expense(groupId, payerId, description, amount, splits, ExpenseType.GROUP);
-            case PERSONAL, NON_GROUP ->
-                    new Expense(UUID.randomUUID(), payerId, description, amount, of(splits.get(0)), ExpenseType.PERSONAL);
-            default -> new InvalidExpenseTypeException("Invalid expense type");
+            case GROUP -> new Expense(groupId, payerId, description, amount, splits);
+            case PERSONAL, NON_GROUP -> new Expense(UUID.randomUUID(), payerId, description, amount, splits);
+            default -> throw new InvalidExpenseTypeException("Invalid expense type");
         };
     }
 }
